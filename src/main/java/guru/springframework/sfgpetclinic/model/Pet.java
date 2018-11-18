436b503333c4dcbca7c3bcf83ec0a6643d271e18
Francislainy <francislainy.campos@gmail.com>
2018-11-18 16:24:34 +0000

    Adding POJOs.

---
+++ src/main/java/guru/springframework/sfgpetclinic/model/Pet.java
@@ -0,0 +1,34 @@
+package guru.springframework.sfgpetclinic.model;
+
+import java.time.LocalDate;
+
+public class Pet {
+
+    private PetType PetYpe;
+    private Owner owner;
+    private LocalDate birthDate;
+
+    public PetType getPetYpe() {
+        return PetYpe;
+    }
+
+    public void setPetYpe(PetType petYpe) {
+        PetYpe = petYpe;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Owner owner) {
+        this.owner = owner;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+}
